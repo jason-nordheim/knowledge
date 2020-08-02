61dc7352a8566292e2a32430df62d0d6e2afb0b5
jason-nordheim <jason.nordheim@gmail.com>
2020-08-02 15:57:59 -0600

    setup PostForm.js to make post request to "https://jsonplaceholder.typicode.com/posts"

--- redux/redux-tutorial-tm/src/Components/PostForm.js
+++ redux/redux-tutorial-tm/src/Components/PostForm.js
@@ -1,7 +1,24 @@
 import React, { useEffect, useState, Fragment } from "react";
 
 export const PostForm = () => {
-  
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const onSubmit = e => {
+    e.preventDefault() 
+    
+    const post = { title, body } 
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST", 
+      headers: {
+        "content-type": "application/json"
+      }, 
+      body: JSON.stringify(post) 
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
 
   return (
     <div>
@@ -9,15 +26,15 @@ export const PostForm = () => {
       <form>
         <div>
           <label>Title: </label><br /> 
-          <input name="title" type="text" /> 
+          <input name="title" type="text" value={title} onChange={e => setTitle(e.target.value)}/> 
         </div>
         <div>
           <label>Body</label><br /> 
-          <textarea name="body" type="text" /> 
+          <textarea name="body" type="text" value={body} onChange={e => setBody(e.target.value)}/> 
         </div>
         <br /> 
         <div>
-          <button type="submit">Submit</button>
+          <button type="submit" onClick={onSubmit}>Submit</button>
         </div>
       </form>
     </div>
