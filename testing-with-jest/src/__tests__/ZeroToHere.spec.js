af9a244815f61df1f5e1992df96b5ab825881afb
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-04 14:40:50 -0600

    wrote test to make sure the data objects have all the relevant properties

--- testing-with-jest/src/__tests__/ZeroToHere.spec.js
+++ testing-with-jest/src/__tests__/ZeroToHere.spec.js
@@ -3,7 +3,28 @@ const pizzas = require('../data.json')
 /**
  * Snapshot testing (the json data file)
  */
-test('the pizza data is correct', () => {
-     // will fail if the data.json file has changed 
-    expect(pizzas).toMatchSnapshot()
+test('The pizza data to be correct', () => {
+     /** will fail if the data.json file has changed from the snapshot */ 
+     expect(pizzas).toMatchSnapshot()
+    
+     /* 1 should always be equal to 1 */ 
+    expect(1).toEqual(1)
+
+    /* we expect all the pizza names to match the follwoing array */
+    expect(pizzas.map( pizza => pizza.name)).toEqual([
+        "Chicago Pizza",
+        "Neopolitan Pizza", 
+        "New York Pizza", 
+        "Sicilian Pizza" 
+    ])
 })
+
+for (let i = 0; i < pizzas.length; i++) {
+    test(`pizza[${i}] should have properties (id, name, image, desc, price)`, () => {
+        expect(pizzas[i]).toHaveProperty('id')
+        expect(pizzas[i]).toHaveProperty('name')
+        expect(pizzas[i]).toHaveProperty('image')
+        expect(pizzas[i]).toHaveProperty('desc')
+        expect(pizzas[i]).toHaveProperty('price')
+    })    
+}
