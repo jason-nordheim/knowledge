33f2332dd796e646055453e921b1dda6a0146aa4
Jason Nordheim <jason.nordheim@gmail.com>
2020-08-04 15:07:41 -0600

    added mocking of functions

--- testing-with-jest/src/__tests__/ZeroToHere.spec.js
+++ testing-with-jest/src/__tests__/ZeroToHere.spec.js
@@ -1,8 +1,9 @@
+// https://jestjs.io/
 const pizzas = require('../data.json') 
 
-/**
- * Snapshot testing (the json data file)
- */
+
+
+/** Snapshot testing (the json data file) */
 test('The pizza data to be correct', () => {
      /** will fail if the data.json file has changed from the snapshot */ 
      expect(pizzas).toMatchSnapshot()
@@ -28,3 +29,71 @@ for (let i = 0; i < pizzas.length; i++) {
         expect(pizzas[i]).toHaveProperty('price')
     })    
 }
+
+// ---------------------
+// mocking 
+// ---------------------
+test('Mock implementation of a basic function', () => {
+    const mock = jest.fn(() => 'I am a mock function')
+    // console.log(mock) // => Will show all the functions of the mock object 
+    expect(mock('Calling my mock function!')).toBe('I am a mock function')
+    expect(mock).toHaveBeenCalledWith("Calling my mock function!")
+})
+
+test('mock return value of a function one time', () => {
+    const mock = jest.fn()
+
+    mock.mockReturnValueOnce('Hello')
+        .mockReturnValueOnce('there!')
+
+    mock()
+    mock() 
+
+    expect(mock).toHaveBeenCalledTimes(2)
+
+    mock('Hello', 'there', 'Steve')
+    expect(mock).toHaveBeenCalledWith('Hello', 'there', 'Steve')
+
+    mock('Steve')
+    expect(mock).toHaveBeenLastCalledWith('Steve')
+})
+
+test('mock implementation of a function', () => {
+    const mock = jest.fn().mockImplementation(() => 'United Kingdom')
+    expect(mock('Location')).toBe('United Kingdom')
+    expect(mock).toHaveBeenCalledWith('Location')
+})
+
+
+// "spying"
+// spy on the functions of a particular module / method 
+test('spying using original implementation', () => {
+    const pizza = {
+        name: n => `Pizza name: ${n}`, 
+    }
+
+    const spy = jest.spyOn(pizza, 'name')
+    expect(pizza.name('Cheese')).toBe('Pizza name: Cheese')
+    expect(spy).toHaveBeenCalledWith('Cheese')
+})
+
+test('spying using mock implementation', () => {
+    const pizza = {
+        name: n => `Pizza name: ${n}`, 
+    }
+
+    const spy = jest.spyOn(pizza, 'name')
+    spy.mockImplementation(n => 'Crazy pizza!')
+
+
+    expect(pizza.name('Cheese')).toBe('Crazy pizza!')
+    expect(spy).toHaveBeenCalledWith('Cheese')
+
+    /** restores original implementation */
+    spy.mockRestore()
+    expect(pizza.name('Cheese')).toBe('Pizza name: Cheese')
+})
+
+
+
+
